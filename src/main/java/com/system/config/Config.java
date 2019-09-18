89a4ca590ed5c1087163156effb0aa83fdc72a0a
Mhassanmustafa <sp17-bcs-065@cuilahore.edu.pk>
2019-09-19 00:35:23 +0500

    address change

--- src/main/java/com/system/config/Config.java
+++ src/main/java/com/system/config/Config.java
@@ -32,15 +32,15 @@ public class Config {
     public static final String filePath = Paths.get(logFile.toAbsolutePath().toString(),String.format("Config.txt")).toString();
     public static final Path stockDetailPath = Paths.get("\\resturantManagementSystem\\ReportsAndSheets\\StockDetailsReports");  //path of folder where the reports are save
     public static final Font font = FontFactory.getFont(FontFactory.HELVETICA,10, BaseColor.BLACK); //font
-    public static final String address = "House 44 street 20 A chaklala scheme 3 rawalpindi"; //shop address
-    public static final String shopName = "SK Enterprises"; //shop name
+    public static final String address = "Shop no 14, 33 civic center Main Plaza Johar Town Lahore."; //shop address
+    public static final String shopName = "BIG BNG BRGR"; //shop name
     public static final int alignRight = Element.ALIGN_RIGHT; //align right
     public static final int alignCenter = Element.ALIGN_CENTER; //align left
     public static final String lines = "-------------------------------------------------------------------------------" +
             "---------------------------------------------------"; //decoration
     public static final String rotateLines = "-------------------------------------------------------------------------------" +
             "----------------------------------------------------------------------------------------------------------------"; //decoration
-    public static final String contactNumber = "Contact no : 0303-5992887";
+    public static final String contactNumber = "0311-4111029";
     public static final Font boldFont = FontFactory.getFont(FontFactory.HELVETICA, 20, Font.BOLD
             , BaseColor.DARK_GRAY);
     public static final Font simpleFont = FontFactory.getFont(FontFactory.HELVETICA,13, BaseColor.BLACK);
